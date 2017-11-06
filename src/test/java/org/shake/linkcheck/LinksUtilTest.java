05bdb12f6e0a697721b7b5ec92f4939a3e95e180
anshake <anshake@gmail.com>
2017-11-06 21:55:48 +0100

    add test for linksFromArray

--- src/test/java/org/shake/linkcheck/LinksUtilTest.java
+++ src/test/java/org/shake/linkcheck/LinksUtilTest.java
@@ -2,6 +2,7 @@ package org.shake.linkcheck;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import junit.framework.AssertionFailedError;
 import org.junit.Test;
 
 import java.net.URI;
@@ -24,6 +25,31 @@ public class LinksUtilTest
 
         uris = LinksUtil.link("b.c2", jsonNode);
         assertEquals("http://host", uris.iterator().next().toString());
+    }
+
+    @Test
+    public void linksFromArray() throws Exception
+    {
+        String json = "[{\"a\": 1, \"b\": {\"c1\": 22, \"c2\" : \"http://host\"}}, {\"b\": {\"c2\": \"https://host2\"}}]";
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNode = mapper.readValue(json, JsonNode.class);
+        Collection<URI> uris = LinksUtil.link("b.c2", jsonNode);
+
+        assertContainsUri(uris, "http://host");
+        assertContainsUri(uris, "https://host2");
+    }
+
+    private void assertContainsUri(Collection<URI> uris, String strUri)
+    {
+        for (URI uri : uris)
+        {
+            if (uri.toString().equals(strUri))
+            {
+                return;
+            }
+        }
+
+        throw new AssertionFailedError();
     }
 
 }
